08f969af19489ca4b3a67f74de8ce8a6de7ec69e
nialandin <141055146+nialandin@users.noreply.github.com>
2023-11-26 01:45:08 +0300

    Add files via upload

---
+++ week_04/Question_04_12.java
@@ -0,0 +1,15 @@
+package assignments.src.week_04;
+
+import java.util.Scanner;
+
+public class Question_04_12 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+
+
+
+    }
+
+}
+
