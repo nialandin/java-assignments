940f1909ac5a44d0c58ee07c8e58749121b4d8e2
nialandin <141055146+nialandin@users.noreply.github.com>
2023-11-27 20:37:13 +0300

    Add files via upload

---
+++ week_04/Question_04_17_yapılmadı.java
@@ -0,0 +1,21 @@
+package assignments.src.week_04;
+
+import java.util.Scanner;
+
+public class Question_04_17_yapılmadı {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a year : ");
+        int year = input.nextInt();
+
+        System.out.println("Enter a month : ");
+        int month = input.nextInt();
+
+
+        int days = 30;
+
+    }
+}
+
+
